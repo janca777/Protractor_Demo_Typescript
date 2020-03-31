7f15223d0470d37f06644236eb37833df041b116
janca777 <shopping.alias@ymail.com>
2020-03-31 16:21:22 +0200

    Delete google.js

--- google.js
+++
@@ -1,9 +0,0 @@
-function Google() {
-   this.searchInput = element(by.name('q'));
-   this.mapsLink = element(by.xpath("//a[contains(text(),'Maps')]"));
-   this.getURL = function () {
-      browser.get('https://www.google.de/')
-   };
-};
-
-module.exports = new Google();
